1c9562c4ec9ba51d81b7eaacfedd5adf2dd6bcdb
Sayed Husain Alawi <shalawi@mail.fhsu.edu>
2024-04-27 11:00:23 +0300

    Created new app called travelblog, finished directory structure, updated settings.py, and finished models and the migration to database

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    "polls.apps.PollsConfig",
+    'travelblog.apps.TravelblogConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'travelblog' / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,7 +116,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
---
+++ mysite/travelblog/models.py
@@ -0,0 +1,47 @@
+from django.db import models
+
+
+class Country(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class City(models.Model):
+    name = models.CharField(max_length=100)
+    country = models.ForeignKey(Country, on_delete=models.CASCADE, related_name='cities')
+
+    def __str__(self):
+        return f"{self.name}, {self.country.name}"
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    publish_date = models.DateTimeField(auto_now_add=True)
+    summary = models.TextField()
+    content = models.TextField()
+    images = models.ImageField(upload_to='posts_images/', blank=True, null=True)
+    location = models.CharField(max_length=200)
+    city = models.ForeignKey(City, on_delete=models.CASCADE)
+    country = models.ForeignKey(Country, on_delete=models.CASCADE)
+    tags = models.CharField(max_length=100, blank=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Activity(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    duration = models.DurationField()
+    cost = models.DecimalField(max_digits=6, decimal_places=2)
+    location = models.CharField(max_length=200)
+    suitability = models.CharField(max_length=200)
+    city = models.ForeignKey(City, on_delete=models.CASCADE)
+    country = models.ForeignKey(Country, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='activities_images/', blank=True, null=True)
+
+    def __str__(self):
+        return self.name
